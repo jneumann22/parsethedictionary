b8f1e3ae47288c9b03c4c02b00f4e50c048e91c4
Jonathan Neumann <jonathanHneumann@gmail.com>
2019-06-27 19:14:31 -0700

    attempt at writing to CSV

---
+++ app.js
@@ -0,0 +1,53 @@
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const csvWriter = createCsvWriter({
+    path: '/Users/Jon/Projects/parsethedictionary/dictionary.csv',
+    header: [
+        {id: 'word', title: 'WORD'},
+    ]
+});
+
+var wordVerification = function(word) {
+    regexp = /^[A-Z]/;
+    if (word === '') {
+        return false;
+    }
+    if (word === word.toUpperCase()) {
+        if (regexp.test(word)){
+            return true
+        } else {
+            return false;
+        }
+    }
+}
+
+
+var request = require('request');
+var fs = require('fs');
+var readline = require('readline');
+var read_stream = request.get('http://www.gutenberg.org/cache/epub/29765/pg29765.txt')
+
+
+var rl = readline.createInterface({
+    input: read_stream
+});
+
+
+rl.on('line', function(line) {
+    rl.pause()
+    setTimeout(function () {
+        if (wordVerification(line)) {
+            let csvObj = [{word : line}]
+            csvWriter.writeRecords(csvObj);
+            }
+    }, 100)
+    rl.resume()
+    
+            
+}).on('close', function(){
+    rl.close();
+})
+    
+
+
+
+
